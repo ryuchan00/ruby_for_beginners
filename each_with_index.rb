8aa1c92f73d0d7bc1c37fe88e997f812ed5bc6a7
ryuchun00 <leokun0210@gmail.com>
2018-01-20 23:00:32 +0900

    4しょう終わり

---
+++ each_with_index.rb
@@ -0,0 +1,3 @@
+fruits = ['apple', 'orange', 'melon']
+# ブロック引数のiには0,1,2..と要素の添え字が入る
+fruits.each_with_index {|fruit, i| puts "#{i}: #{fruit}"}
---
+++ with_index.rb
@@ -0,0 +1,3 @@
+fruits = ['apple', 'orange', 'melon']
+# mapとして処理しつつ、添え字も受け取る
+fruits.map.with_index { |fruit, i| "#{i}: #{fruit} " }
