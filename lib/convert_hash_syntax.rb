05d2c7a70f7ce7277d6b1066b09d757598929a14
ryuchun00 <leokun0210@gmail.com>
2018-01-22 22:54:49 +0900

    正規表現の例題ができた

--- lib/convert_hash_syntax.rb
+++ lib/convert_hash_syntax.rb
@@ -1,4 +1,3 @@
 def convert_hash_syntax(old_syntax)
-  # 何も変換せずに返す
-  old_syntax
+  old_syntax.gsub(/:(\w+) *=> */, '\1: ')
 end
--- spec/convert_hash_syntax_spec.rb
+++ spec/convert_hash_syntax_spec.rb
@@ -2,6 +2,20 @@ require_relative '../lib/convert_hash_syntax'
 
 describe 'convert_hash_syntax' do
   it '正常' do
-    expect(convert_hash_syntax('{}')).to eq('{}')
+    old_syntax = <<~TEXT
+    {
+      :name => 'Alice',
+      :age => 20,
+      :gender => :female
+    }
+    TEXT
+    expected = <<~TEXT
+    {
+      name: 'Alice',
+      age: 20,
+      gender: :female
+    }
+    TEXT
+    expect(convert_hash_syntax(old_syntax)).to eq(expected)
   end
 end
