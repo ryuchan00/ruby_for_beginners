25818e4e808d5f00fc3c7e77a7599ea30bed3571
ryuchan00 <yamakawa00@pepabo.com>
2018-01-23 11:51:51 +0900

    改札から出場できないテストの仮実装が完了した、次はグリーンにするためにコードかく

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -2,14 +2,27 @@ require_relative '../lib/gate'
 require_relative '../lib/ticket'
 
 describe 'gate' do
-  it '改札を出場できる' do
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
+  context '梅田から十三まで' do
+    it '改札を出場できる' do
+      umeda = Gate.new(:umeda)
+      juso = Gate.new(:juso)
 
-    ticket = Ticket.new(150)
-    umeda.enter('ticket')
-    expect(juso.exit(ticket))
-    # クラスが存在するかの確認は、以下のコードで可能
-    # expect(Gate.new).to be_truthy
+      ticket = Ticket.new(150)
+      umeda.enter('ticket')
+      expect(juso.exit(ticket)).to be_truthy
+      # クラスが存在するかの確認は、以下のコードで可能
+      # expect(Gate.new).to be_truthy
+    end
+  end
+
+  context '梅田から三国まで' do
+    it '改札を出場できない' do
+      umeda = Gate.new(:umeda)
+      mikuni = Gate.new(:mikuni)
+
+      ticket = Ticket.new(150)
+      umeda.enter(ticket)
+      expect(mikuni.exit(ticket)).to be_falsey
+    end
   end
 end
