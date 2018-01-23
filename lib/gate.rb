97e34ef799f732aac4f29e2e3ed97d7f46fe511b
ryuchan00 <yamakawa00@pepabo.com>
2018-01-23 14:01:23 +0900

    rdocを活用してみた。結構いいね

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,20 +1,35 @@
+# 改札機を表すクラス
 class Gate
-  STATIONS = [:umeda, :juso, :mikuni]
-  FARES = [150, 190]
+  STATIONS = [:umeda, :juso, :mikuni] #:nodoc:
+  FARES = [150, 190] #:nodoc:
 
+  # Gateオブジェクトの作成
+  # ==== 引数
+  # *+name+ - 駅名
   def initialize(name)
     @name = name
   end
 
+  # 改札機に通って駅に入場する
+  # ==== 引数
+  # *+ticket+ - 切符
   def enter(ticket)
     ticket.stamp(@name)
   end
 
+  # 改札機を通って駅から出場する
+  # ==== 引数
+  # *+ticket+ - 切符
+  # ==== 戻り値
+  # *+boolean+ - 運賃が足りて入れば+true+、不足して入れば+false+
   def exit(ticket)
     fare = calc_fare(ticket)
     fare <= ticket.fare
   end
 
+  # 運賃を計算する
+  # ==== 引数
+  # *+ticket+ - 切符
   def calc_fare(ticket)
     from = STATIONS.index(ticket.stamped_at)
     to = STATIONS.index(@name)
