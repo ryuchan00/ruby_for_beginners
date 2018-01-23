2a15120ce65920b6b19dc05de1d86e22233eec06
ryuchan00 <yamakawa00@pepabo.com>
2018-01-23 11:28:28 +0900

    Ticketクラスを作成した。まだレッド

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../lib/gate'
+require_relative '../lib/ticket'
 
 describe 'gate' do
-  it 'エラーにならない' do
-    expect(Gate.new).to be_truthy
+  it '改札を出場できる' do
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(150)
+    umeda.enter('ticket')
+    expect(juso.exit(ticket))
+    # クラスが存在するかの確認は、以下のコードで可能
+    # expect(Gate.new).to be_truthy
   end
 end
