772ecfab18cf70e3c2cddde95a61677ec17c41d9
ryuchan00 <yamakawa00@pepabo.com>
2018-01-23 13:39:51 +0900

    不要なコメントアウトを削除した

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -10,22 +10,14 @@ describe 'gate' do
 
   context '梅田から十三まで' do
     it '改札を出場できる' do
-      # umeda = Gate.new(:umeda)
-      # juso = Gate.new(:juso)
-
       ticket = Ticket.new(150)
       @umeda.enter(ticket)
       expect(@juso.exit(ticket)).to be_truthy
-      # クラスが存在するかの確認は、以下のコードで可能
-      # expect(Gate.new).to be_truthy
     end
   end
 
   context '梅田から三国まで' do
     it '改札を出場できない' do
-      # umeda = Gate.new(:umeda)
-      # mikuni = Gate.new(:mikuni)
-
       ticket = Ticket.new(150)
       @umeda.enter(ticket)
       expect(@mikuni.exit(ticket)).to be_falsey
