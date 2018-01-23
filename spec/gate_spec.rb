51aae29f805f6ed6d502a86e31301545580c9867
ryuchan00 <yamakawa00@pepabo.com>
2018-01-23 13:31:45 +0900

    Gateオブジェクトの生成を一つのブロックにまとめてある

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -2,14 +2,20 @@ require_relative '../lib/gate'
 require_relative '../lib/ticket'
 
 describe 'gate' do
+  before do
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+
   context '梅田から十三まで' do
     it '改札を出場できる' do
-      umeda = Gate.new(:umeda)
-      juso = Gate.new(:juso)
+      # umeda = Gate.new(:umeda)
+      # juso = Gate.new(:juso)
 
       ticket = Ticket.new(150)
-      umeda.enter(ticket)
-      expect(juso.exit(ticket)).to be_truthy
+      @umeda.enter(ticket)
+      expect(@juso.exit(ticket)).to be_truthy
       # クラスが存在するかの確認は、以下のコードで可能
       # expect(Gate.new).to be_truthy
     end
@@ -17,12 +23,12 @@ describe 'gate' do
 
   context '梅田から三国まで' do
     it '改札を出場できない' do
-      umeda = Gate.new(:umeda)
-      mikuni = Gate.new(:mikuni)
+      # umeda = Gate.new(:umeda)
+      # mikuni = Gate.new(:mikuni)
 
       ticket = Ticket.new(150)
-      umeda.enter(ticket)
-      expect(mikuni.exit(ticket)).to be_falsey
+      @umeda.enter(ticket)
+      expect(@mikuni.exit(ticket)).to be_falsey
     end
   end
 end
