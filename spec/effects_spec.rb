bdc192d9a695c23751e87e6e2fd1d6d5aed892fd
ryuchun00 <leokun0210@gmail.com>
2018-02-01 23:04:20 +0900

    スペースで分割 > 大文字変換と"!"の付与 > スペースで連結

--- spec/effects_spec.rb
+++ spec/effects_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/effects'
 describe 'reverse' do
   let(:effect) { Effects.reverse }
   subject(:effect_reverse) { effect.call('Ruby is fun!') }
-  it 'とりあえずモジュールが参照できることを確認する' do
+  it '文字列がシャッフルされる' do
     expect(effect_reverse).to eq('ybuR si !nuf')
   end
 end
@@ -11,7 +11,7 @@ end
 describe 'echo' do
   subject(:effect_echo) { effect.call('Ruby is fun!') }
 
-  context '繰り返し回数が2の場合' do
+  context '引数が2の場合' do
     let(:effect) { Effects.echo(2) }
 
     it '各文字が2回づつ繰り返される' do
@@ -19,11 +19,32 @@ describe 'echo' do
     end
   end
 
-  context '繰り返し回数が3の場合' do
+  context '引数が3の場合' do
     let(:effect) { Effects.echo(3) }
 
     it '各文字が3回づつ繰り返される' do
       expect(effect_echo).to eq('RRRuuubbbyyy iiisss fffuuunnn!!!')
+    end
+  end
+end
+
+describe 'loud' do
+  let(:effect) { Effects.loud(arg) }
+  subject(:effect_loud) { effect.call('Ruby is fun!') }
+
+  context '引数が2の場合' do
+    let(:arg) { 2 }
+
+    it '文字列が大文字になり、単語毎の末尾に`!!`がつく' do
+      expect(effect_loud).to eq('RUBY!! IS!! FUN!!!')
+    end
+  end
+
+  context '引数が3の場合' do
+    let(:arg) { 3 }
+
+    it '文字列が大文字になり、単語毎の末尾に`!!!`がつく' do
+      expect(effect_loud).to eq('RUBY!!! IS!!! FUN!!!!')
     end
   end
 end
